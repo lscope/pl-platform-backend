07d76e3fe7fbd257f24c8b99e19671dd6d33b1e9
lscope <l.scope98@gmail.com>
2024-03-19 08:13:05 +0000

    [fix] db connections

--- pl_backend/__init__.py
+++ pl_backend/__init__.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask
 from flask_restful import Api
+from sqlalchemy.exc import OperationalError
+from time import sleep
+
 from .resources.user import UserResource
 from .models import db
 from .models.lifts import (
@@ -28,8 +31,22 @@ def create_app() -> Flask:
 
     # Create DB tables if not exist
     with app.app_context():
-        db.create_all()
+        # Try to connect to db 10 times, waiting 5 seconds between each
+        calls = 1
 
+        while calls <= 10:
+            try:
+                db.create_all()
+                break
+            except OperationalError:
+                print("Can't connect to DB, wait 5 seconds and try again..")
+                sleep(5)
+                calls += 1
+
+        if calls > 10:
+            raise Exception("Can't connect to DB")
+
+    # Add APIs
     api.add_resource(UserResource, "/user")
 
     return app
