5a17d36bc6b057136f29fb7e44d8cb4c15422fd0
lscope <l.scope98@gmail.com>
2024-03-29 12:21:13 +0000

    [add] foreignKey ondelete option

--- pl_backend/models/lift.py
+++ pl_backend/models/lift.py
@@ -8,7 +8,7 @@ class Lift(Base): # Obbligatorio che la classe estenda Base
     __tablename__ = "lifts" # Questo il nome effettivo della tabella nel DB
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False) # Con l'opzione ondelete indichiamo che se il parent viene cancellato, tutti i figli vengono cancellati (quindi se uno user viene cancellato, tutti i suoi pesi vengono cancellati)
     weight = Column(Float, nullable=False)
     registered_dt = Column(Date, default=date.today())
 
