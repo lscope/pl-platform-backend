757911aa7fb28de8a03f86bb63b307c7111c46b4
lscope <l.scope98@gmail.com>
2024-03-29 07:47:41 +0000

    [fix] user creation time

--- pl_backend/models/user.py
+++ pl_backend/models/user.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
+from sqlalchemy.sql import func
 from . import Base
 
 
@@ -11,4 +12,4 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    register_dt = Column(DateTime, default=datetime.now())
+    register_dt = Column(DateTime, default=func.now())
